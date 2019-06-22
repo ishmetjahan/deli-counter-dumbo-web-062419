5b13ec4201778616dcd366a0aad9830ef03d0223
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 04:27:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def line (katzDeli)
+if katzDeli.length >=1 
+  currentLine= "The "
+  katzDeli.each.with_index(1) do |ele,idx|
+    newArr.push("#{count}. #{ele}")
+  count+=1 
+end
+puts "The line is currently:#{newArr.join(" ")}"
+else
+  puts "The line is currently empty."
+end
+end 
+#
+#
+#
+def take_a_number(katzDeli, name)
+  katzDeli.push(name)
+  puts "Welcome, #{name}. You are number #{katzDeli.length} in line"
+end
+#
+#
+#
